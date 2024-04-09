fd5463c36f3c5d12c8aaca79ad6d7fae6dca13c8
Leonardo Leao <leonardodleao@gmail.com>
2024-04-09 03:36:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,27 +19,87 @@ const barbearia = {
   
   
 function buscaCortePorId(id) {
+    for(let i = 0; i <barbearia.cortes.length; i++){
+        if(id == barbearia.cortes[i].id){
+            return barbearia.cortes[i];
+        } 
+    }
+    return "corte não encontrado"
 }
 
 function buscaBarbaPorId(id) {
+    for(let i = 0; i <barbearia.barbas.length; i++){
+        if(id == barbearia.barbas[i].id){
+            return barbearia.barbas[i];
+        } 
+    }
+    return "Barba não encontrada"
 }
 
 function verificaStatusBarbearia() {
+    if(barbearia.estaAberto == true){
+        return "Estamos abertos";
+    }
+    return "Estamos fechados"
 }
 
 function retornaTodosCortes() {
+    return barbearia.cortes
 }
 
 function retornaTodasBarbas() {
+    return barbearia.barbas
 }
 
 function criaPedido(nomeCliente, corteId, barbaId) {
-}
+    let pedido = 
+    {nome: nomeCliente, 
+    pedidoCorte: buscaCortePorId,
+    pedidoCortePreco: 0,
+    pedidoBarba: buscaBarbaPorId,
+    pedidoBarbaPreco: 0,
+    }
+    
 
-function atualizarServico(lista, id, valor, tipo) {
-}
+    for(let i = 0; i < barbearia.cortes.length; i++){
+        if(corteId == barbearia.cortes[i].id){
+            pedido.pedidoCorte = barbearia.cortes[i].tipo;
+            pedido.pedidoCortePreco = barbearia.cortes[i].valor;
+        }
+    }
+    for(let j = 0; j < barbearia.cortes.length; j++){
+        if(barbaId == barbearia.barbas[j].id){
+            pedido.pedidoBarba = barbearia.barbas[j].tipo;
+            pedido.pedidoBarbaPreco = barbearia.barbas[j].valor;
+        }
+    }
+    return pedido;
+ }
+
+
+ function atualizarServico(lista, id, tipo, valor) 
+    // let novoId = id;
+    
+//     for (let i = 0; i < lista.length; i++) {
+//         if(lista[i].id == id){
+//         lista[i].id.pull(novoId); 
+//         if(lista[i].id == id){
+//         lista[i].tipo.pull(tipo);
+//         if(lista[i].id == id){
+//         lista[i].valor.pull(valor);
+//         break;
+//         }
+//     } 
+// }
+//     } 
+// return lista;
+// }
 
 function calculaTotal(pedido) {
+    let precoCorte = pedido.pedidoCortePreco;
+    let precoBarba = pedido.pedidoBarbaPreco;
+    console.log(pedido)
+    return  precoBarba + precoCorte;
 }
   
   
